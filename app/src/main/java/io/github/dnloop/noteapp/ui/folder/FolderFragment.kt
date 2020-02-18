ba261218905dc21b657e004e1318ca77c395c42a
dnloop <29187104+dnloop@users.noreply.github.com>
2020-02-18 11:03:25 -0300

    Folder shows notes by category. Added destinations from folder.

--- app/src/main/java/io/github/dnloop/noteapp/ui/folder/FolderFragment.kt
+++ app/src/main/java/io/github/dnloop/noteapp/ui/folder/FolderFragment.kt
@@ -1,4 +1,4 @@
-package io.github.dnloop.noteapp.ui.note
+package io.github.dnloop.noteapp.ui.folder
 
 
 import android.os.Bundle
@@ -6,19 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import io.github.dnloop.noteapp.R
 
 /**
  * A simple [Fragment] subclass.
  */
-class ListNoteFragment : Fragment() {
+class FolderFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_notes, container, false)
+        val root = inflater.inflate(R.layout.fragment_folder, container, false)
 
         return root
     }
--- app/src/main/java/io/github/dnloop/noteapp/ui/home/HomeFragment.kt
+++ app/src/main/java/io/github/dnloop/noteapp/ui/home/HomeFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import io.github.dnloop.noteapp.R
+import io.github.dnloop.noteapp.adapter.NoteAdapter
+import io.github.dnloop.noteapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
@@ -20,9 +24,19 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val binding: FragmentHomeBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home, container, false)
 
+        val adapter = NoteAdapter()
 
+        binding.noteList.adapter = adapter
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        homeViewModel.getNotes().observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
+
+        return binding.root
     }
 }
