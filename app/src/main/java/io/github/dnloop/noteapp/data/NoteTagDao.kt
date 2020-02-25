f7c41898422a1584d6319fc80b90c394aea7f774
dnloop <29187104+dnloop@users.noreply.github.com>
2020-02-25 17:48:32 -0300

    Removed unused import.

--- app/src/main/java/io/github/dnloop/noteapp/data/NoteTagDao.kt
+++ app/src/main/java/io/github/dnloop/noteapp/data/NoteTagDao.kt
@@ -1,7 +1,9 @@
 package io.github.dnloop.noteapp.data
 
-import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 @Dao
 interface NoteTagDao {
