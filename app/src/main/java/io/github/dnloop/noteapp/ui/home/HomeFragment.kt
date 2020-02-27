966da4d1a808f644607ac5b73267775a2492164b
dnloop <29187104+dnloop@users.noreply.github.com>
2020-02-27 20:29:41 -0300

    Added Coroutines, ViewModelFactory, temporary dummy data.

--- app/src/main/java/io/github/dnloop/noteapp/ui/home/HomeFragment.kt
+++ app/src/main/java/io/github/dnloop/noteapp/ui/home/HomeFragment.kt
@@ -10,30 +10,39 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import io.github.dnloop.noteapp.R
 import io.github.dnloop.noteapp.adapter.NoteAdapter
+import io.github.dnloop.noteapp.data.NoteDatabase
 import io.github.dnloop.noteapp.databinding.FragmentHomeBinding
 import io.github.dnloop.noteapp.ui.viewmodel.NoteViewModel
+import io.github.dnloop.noteapp.ui.viewmodel.NoteViewModelFactory
 
 class HomeFragment : Fragment() {
-
-    private lateinit var homeViewModel: NoteViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(NoteViewModel::class.java)
         val binding: FragmentHomeBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_home, container, false)
+
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = NoteDatabase.getInstance(application).noteDao
+
+        val viewModelFactory = NoteViewModelFactory(dataSource, application)
+
+        val homeViewModel =
+            ViewModelProvider(this, viewModelFactory).get(NoteViewModel::class.java)
 
         val adapter = NoteAdapter()
 
         binding.noteList.adapter = adapter
 
+        binding.homeViewModel = homeViewModel
+
         binding.lifecycleOwner = this
 
-        homeViewModel.getNotes().observe(viewLifecycleOwner, Observer {
+        homeViewModel.allNotes.observe(viewLifecycleOwner, Observer {
             it?.let {
                 adapter.data = it
             }
--- app/src/main/java/io/github/dnloop/noteapp/ui/viewmodel/NoteViewModel.kt
+++ app/src/main/java/io/github/dnloop/noteapp/ui/viewmodel/NoteViewModel.kt
@@ -4,32 +4,58 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import io.github.dnloop.noteapp.data.Note
-import io.github.dnloop.noteapp.data.NoteDatabase
-import io.github.dnloop.noteapp.data.NoteRepository
-import io.github.dnloop.noteapp.data.NotesWithCategory
+import io.github.dnloop.noteapp.data.*
 import kotlinx.coroutines.*
+import javax.sql.DataSource
 
-class NoteViewModel(application: Application) : AndroidViewModel(application) {
+class NoteViewModel(private val dataSource: NoteDao, application: Application) : AndroidViewModel(application) {
 
-    private val repository: NoteRepository
-    private lateinit var _allNotes: LiveData<List<Note>>
+    private lateinit var repository: NoteRepository
     private lateinit var _notesWithCategory: LiveData<List<NotesWithCategory>>
+    val allNotes: LiveData<List<Note>> = dataSource.getAllNotes()
     private var viewModelJob = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     init {
-        val noteDao = NoteDatabase.getInstance(application).noteDao
-        repository = NoteRepository(noteDao)
+        initializeRepository()
+    }
+
+    private fun addDummyData() {
+        uiScope.launch { withContext(Dispatchers.IO) {
+            repository.clearTable()
+            // Add sample words.
+            val note = Note()
+            note.id = 1
+            note.title = "title 1"
+            note.content = "content 1"
+            note.createdAt = System.currentTimeMillis()
+            note.modifiedAt = System.currentTimeMillis()
+            repository.insert(note)
+            note.id = 2
+            note.title = "title 2"
+            note.content = "content 2"
+            note.createdAt = System.currentTimeMillis()
+            note.modifiedAt = System.currentTimeMillis()
+            repository.insert(note)
+        } }
+    }
+
+    private fun initializeRepository() {
+        uiScope.launch {
+            repository = getRepository()
+            addDummyData()
+        }
+    }
+
+    private suspend fun getRepository(): NoteRepository {
+        return withContext(Dispatchers.IO) {
+           NoteRepository(dataSource)
+        }
     }
 
     override fun onCleared() {
         super.onCleared()
         viewModelJob.cancel()
-    }
-
-    fun getNotes(): LiveData<List<Note>> {
-        return allNotes
     }
 
     fun onInsert(note: Note) {
@@ -48,15 +74,9 @@ class NoteViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
-    private val allNotes: MutableLiveData<List<Note>> by lazy {
-        MutableLiveData<List<Note>>().also { loadNotes() }
-    }
-
-    private fun loadNotes() {
-        uiScope.launch {
-            _allNotes = withContext(Dispatchers.IO) {
-                repository.allNotes
-            }
+    private suspend fun loadNotes(): LiveData<List<Note>> {
+        return withContext(Dispatchers.IO) {
+            repository.allNotes
         }
     }
 
