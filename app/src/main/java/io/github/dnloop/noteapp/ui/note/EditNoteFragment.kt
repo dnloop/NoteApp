e5d7240ffe941e5b9fce533978e1d4e53ce1f593
dnloop <29187104+dnloop@users.noreply.github.com>
2020-02-28 18:02:09 -0300

    Added binding and ViewModel to the Note Editor.

--- app/src/main/java/io/github/dnloop/noteapp/ui/note/EditNoteFragment.kt
+++ app/src/main/java/io/github/dnloop/noteapp/ui/note/EditNoteFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import io.github.dnloop.noteapp.R
+import io.github.dnloop.noteapp.data.NoteDatabase
+import io.github.dnloop.noteapp.databinding.FragmentNoteEditorBinding
+import io.github.dnloop.noteapp.ui.viewmodel.NoteViewModel
+import io.github.dnloop.noteapp.ui.viewmodel.NoteViewModelFactory
 
 class EditNoteFragment : Fragment() {
 
@@ -13,8 +19,18 @@ class EditNoteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_note_editor, container, false)
+        val binding: FragmentNoteEditorBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_note_editor, container, false)
 
-        return root
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = NoteDatabase.getInstance(application).noteDao
+
+        val viewModelFactory = NoteViewModelFactory(dataSource, application)
+
+        val noteViewModel =
+            ViewModelProvider(this, viewModelFactory).get(NoteViewModel::class.java)
+
+        return binding.root
     }
 }
