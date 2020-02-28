39e69cb0e878832f181dd9638a8c33986d5bf62b
dnloop <29187104+dnloop@users.noreply.github.com>
2020-02-28 11:59:53 -0300

    Display custom item, fixed minor view glitches.

--- app/src/main/java/io/github/dnloop/noteapp/adapter/NoteAdapter.kt
+++ app/src/main/java/io/github/dnloop/noteapp/adapter/NoteAdapter.kt
@@ -3,15 +3,17 @@ package io.github.dnloop.noteapp.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ExpandableListView
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.mikhaellopez.circularimageview.CircularImageView
 import io.github.dnloop.noteapp.R
+
+import io.github.dnloop.noteapp.auxiliary.Formatter
+import io.github.dnloop.noteapp.auxiliary.Validator
 import io.github.dnloop.noteapp.data.Note
 
-class NoteAdapter : RecyclerView.Adapter<TextItemViewHolder>()  {
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.ViewHolder>()  {
 
     var data= listOf<Note>()
         set(value) {
@@ -21,17 +23,31 @@ class NoteAdapter : RecyclerView.Adapter<TextItemViewHolder>()  {
 
     override fun getItemCount() = data.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
+            .inflate(R.layout.list_item_note, parent, false)
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.title
+        val res = holder.itemView.resources
+//        holder.circularImageView.apply { circleColor = Color.GREEN }
+        holder.textTitle.text = item.title
+        holder.textBody.text = item.content
+        holder.textCategory.text = Validator.isCategoryNull(item.categoryId)
+        holder.textModifiedAt.text = Formatter.longToDate(item.modifiedAt)
     }
 
+
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val circularImageView: CircularImageView = itemView.findViewById(R.id.circularImageView)
+        val textTitle: TextView = itemView.findViewById(R.id.text_title)
+        val textBody: TextView = itemView.findViewById(R.id.text_body)
+        val textCategory: TextView = itemView.findViewById(R.id.text_category)
+        val textModifiedAt: TextView = itemView.findViewById(R.id.text_modified_at)
+    }
 }
 
 private class NoteDiffCallback : DiffUtil.ItemCallback<Note>() {
@@ -48,10 +64,3 @@ private class NoteDiffCallback : DiffUtil.ItemCallback<Note>() {
 class NoteListener(val clickListener: (noteId: Int) -> Unit) {
     fun onClick(note: Note) = clickListener(note.id)
 }
-
-/**
- * ViewHolder that holds a single [TextView].
- * Needs optimizations, future removal
- */
-
-class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
