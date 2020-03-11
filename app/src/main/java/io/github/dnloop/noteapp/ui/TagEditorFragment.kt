cc93b72972a18516d33c62ea91eb4afe295b449c
dnloop <29187104+dnloop@users.noreply.github.com>
2020-03-11 10:28:33 -0300

    Added TagEditor.

---
+++ app/src/main/java/io/github/dnloop/noteapp/ui/TagEditorFragment.kt
@@ -0,0 +1,21 @@
+package io.github.dnloop.noteapp.ui
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.github.dnloop.noteapp.R
+import io.github.dnloop.noteapp.ui.viewmodel.TagEditorViewModel
+
+
+class TagEditorFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/io/github/dnloop/noteapp/ui/viewmodel/TagEditorViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.dnloop.noteapp.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class TagEditorViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
