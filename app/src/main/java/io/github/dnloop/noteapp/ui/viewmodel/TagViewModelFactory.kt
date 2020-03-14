828ca6d18fc10916ea788c63b48985721b4d602b
dnloop <29187104+dnloop@users.noreply.github.com>
2020-03-14 11:36:47 -0300

    Removed tag data source.

--- app/src/main/java/io/github/dnloop/noteapp/ui/viewmodel/TagViewModelFactory.kt
+++ app/src/main/java/io/github/dnloop/noteapp/ui/viewmodel/TagViewModelFactory.kt
@@ -8,13 +8,12 @@ import io.github.dnloop.noteapp.data.TagDao
 @Suppress("UNCHECKED_CAST")
 class TagViewModelFactory (
     private val noteKeyId: Long,
-    private val tagDataSource: TagDao,
+    private val noteDataSource: NoteTagDao,
     private val noteTagDataSource: NoteTagDao
 ): ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(TagViewModel::class.java)) {
-            return TagViewModel(noteKeyId, tagDataSource, noteTagDataSource) as T
-            // TODO maybe remove the tag data source because its only attaching/detaching
+            return TagViewModel(noteKeyId, noteDataSource, noteTagDataSource) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
