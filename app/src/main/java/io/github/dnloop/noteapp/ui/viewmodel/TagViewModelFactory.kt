ec1d43540a479cd5be70da1470d6ed55cfcf4144
dnloop <29187104+dnloop@users.noreply.github.com>
2020-03-14 11:39:32 -0300

    Fixed Wrong type.

--- app/src/main/java/io/github/dnloop/noteapp/ui/viewmodel/TagViewModelFactory.kt
+++ app/src/main/java/io/github/dnloop/noteapp/ui/viewmodel/TagViewModelFactory.kt
@@ -2,13 +2,14 @@ package io.github.dnloop.noteapp.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.github.dnloop.noteapp.data.NoteDao
 import io.github.dnloop.noteapp.data.NoteTagDao
 import io.github.dnloop.noteapp.data.TagDao
 
 @Suppress("UNCHECKED_CAST")
 class TagViewModelFactory (
     private val noteKeyId: Long,
-    private val noteDataSource: NoteTagDao,
+    private val noteDataSource: NoteDao,
     private val noteTagDataSource: NoteTagDao
 ): ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
