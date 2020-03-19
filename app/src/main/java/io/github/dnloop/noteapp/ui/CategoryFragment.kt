344f9ab66a6be8dce64ad48a291404e156d4c0ee
dnloop <29187104+dnloop@users.noreply.github.com>
2020-03-19 11:23:16 -0300

    Adjusted Category Implementation.

--- app/src/main/java/io/github/dnloop/noteapp/ui/CategoryFragment.kt
+++ app/src/main/java/io/github/dnloop/noteapp/ui/CategoryFragment.kt
@@ -4,24 +4,118 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import io.github.dnloop.noteapp.MainActivity
 import io.github.dnloop.noteapp.R
+import io.github.dnloop.noteapp.adapter.CategoryAdapter
+import io.github.dnloop.noteapp.adapter.CategoryListener
+import io.github.dnloop.noteapp.data.Category
+import io.github.dnloop.noteapp.data.NoteDatabase
+import io.github.dnloop.noteapp.databinding.FragmentCategoryBinding
 import io.github.dnloop.noteapp.ui.viewmodel.CategoryViewModel
+import io.github.dnloop.noteapp.ui.viewmodel.CategoryViewModelFactory
+import timber.log.Timber
 
-class CategoryFragment : Fragment() {
+class CategoryFragment : Fragment(), CategoryDialogFragment.CategoryDialogListener {
+
+    private lateinit var binding: FragmentCategoryBinding
 
     private lateinit var categoryViewModel: CategoryViewModel
+
+    private var _category: Category = Category()
+
+    private fun showCategoryDialog(Category: Category) {
+        val dialog = CategoryDialogFragment(Category)
+        dialog.listener = this
+        dialog.show(childFragmentManager, "CategoryDialogFragment")
+    }
+
+    override fun onDialogPositiveClick(dialog: DialogFragment, category: Category) {
+        categoryViewModel.onUpdate(category)
+        Toast.makeText(context, "Category edited", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDialogNegativeClick(dialog: DialogFragment) {}
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        categoryViewModel =
-                ViewModelProviders.of(this).get(CategoryViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_category, container, false)
+        categoryViewModel = init()
 
-        return root
+        val adapter =  CategoryAdapter(CategoryListener {
+            catId -> categoryViewModel.onCategorySelected(catId)
+        })
+
+        adapter.clickListener.setOnDeleteClickListener { category ->
+            category.let {
+                categoryViewModel.onDelete(it)
+                Toast.makeText(context, "Tag deleted: ${it.name}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_category, container, false
+        )
+
+        binding.setBinding(adapter)
+
+        categoryViewModel.categoryDataSource.getAllCategories().observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
+
+        categoryViewModel.openDialogEditor.observe(viewLifecycleOwner, Observer {tag ->
+            tag?.let {
+                showTagDialog(it)
+            }
+        })
+
+        return binding.root
+    }
+
+    private fun showTagDialog(category: Category) {
+        val dialog = CategoryDialogFragment(category)
+        dialog.listener = this
+        dialog.show(childFragmentManager, "CategoryDialogFragment")
+    }
+
+    private fun FragmentCategoryBinding.setBinding(adapter: CategoryAdapter) {
+        (activity as MainActivity?)?.getFloatingActionButton()?.hide()
+
+        binding.categoryList.adapter = adapter
+
+        binding.categoryViewModel = this@CategoryFragment.categoryViewModel
+
+        binding.lifecycleOwner = this@CategoryFragment
+
+        btnAdd.setOnClickListener {
+            checkCategory()
+        }
+    }
+
+    private fun checkCategory() {
+        _category.name = binding.categoryInput.text.toString()
+        if (_category.name.isNotBlank()) {
+            categoryViewModel.onInsert(_category)
+            Timber.i("Tag '${_category.name}' Inserted")
+        }
+    }
+
+    private fun init(): CategoryViewModel {
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = NoteDatabase.getInstance(application).categoryDao
+
+        val viewModelFactory = CategoryViewModelFactory(dataSource, application)
+
+        return ViewModelProvider(this, viewModelFactory).get(CategoryViewModel::class.java)
     }
 }
--- app/src/main/java/io/github/dnloop/noteapp/ui/viewmodel/CategoryViewModel.kt
+++ app/src/main/java/io/github/dnloop/noteapp/ui/viewmodel/CategoryViewModel.kt
@@ -2,28 +2,59 @@ package io.github.dnloop.noteapp.ui.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import io.github.dnloop.noteapp.data.Category
 import io.github.dnloop.noteapp.data.CategoryDao
+import io.github.dnloop.noteapp.data.CategoryRepository
+import kotlinx.coroutines.*
 
 class CategoryViewModel(val categoryDataSource: CategoryDao, application: Application) : AndroidViewModel(application) {
 
-    private val notes: MutableLiveData<List<Category>> by lazy {
-        MutableLiveData<List<Category>>().also {
-            loadCategories()
+    private val _openDialogEditor = MutableLiveData<Category>()
+
+    val openDialogEditor
+        get() = _openDialogEditor
+
+    fun onOpenDialogEditorNavigated() {
+        _openDialogEditor.value = null
+    }
+
+    fun onCategorySelected(item: Category) {
+        _openDialogEditor.value = item
+    }
+
+    private suspend fun getRepository(): CategoryRepository {
+        return withContext(Dispatchers.IO) {
+            CategoryRepository(categoryDataSource)
         }
     }
 
-    fun getCategories(): LiveData<List<Category>> {
-        return notes
+    override fun onCleared() {
+        super.onCleared()
+        Job().cancel()
     }
 
-    private fun loadCategories() {
-        // Do an asynchronous operation to fetch categories.
+    fun onInsert(category: Category) {
+        CoroutineScope(Dispatchers.Main + Job()).launch {
+            withContext(Dispatchers.IO) {
+                getRepository().insert(category)
+            }
+        }
     }
 
     fun onUpdate(category: Category) {
+        CoroutineScope(Dispatchers.Main + Job()).launch {
+            withContext(Dispatchers.IO) {
+                getRepository().update(category)
+            }
+        }
+    }
+
+    fun onDelete(category: Category) {
+        CoroutineScope(Dispatchers.Main + Job()).launch {
+            withContext(Dispatchers.IO) {
+                getRepository().hardDelete(category)
+            }
+        }
     }
 }
