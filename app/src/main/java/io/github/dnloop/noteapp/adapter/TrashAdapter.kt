76058d13171d0522793ce5af303edf2bdb84c8e2
dnloop <29187104+dnloop@users.noreply.github.com>
2020-04-06 16:53:21 -0300

    Added trash adapter logic.

--- app/src/main/java/io/github/dnloop/noteapp/adapter/TrashAdapter.kt
+++ app/src/main/java/io/github/dnloop/noteapp/adapter/TrashAdapter.kt
@@ -1,8 +1,54 @@
 package io.github.dnloop.noteapp.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import io.github.dnloop.noteapp.data.Note
+import io.github.dnloop.noteapp.databinding.ListItemTrashBinding
 
-class TrashAdapter {
+class TrashAdapter(val clickListener: TrashListener): ListAdapter<Note, TrashAdapter.ViewHolder>(TrashDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position)!!, clickListener)
+    }
+
+    class ViewHolder private constructor(val binding: ListItemTrashBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(
+            item: Note,
+            clickListener: TrashListener
+        ) {
+            binding.note = item
+            binding.clickListener = clickListener
+            binding.btnHardDelete.setOnClickListener {
+                clickListener.listener?.invoke(item)
+            }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListItemTrashBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+    private class TrashDiffCallback : DiffUtil.ItemCallback<Note>() {
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem == newItem
+        }
+    }
+
 }
 
 
