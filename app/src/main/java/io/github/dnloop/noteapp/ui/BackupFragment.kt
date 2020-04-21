e42702c276c903941d5d1fe73287898242098b11
dnloop <29187104+dnloop@users.noreply.github.com>
2020-04-21 16:49:26 -0300

    Added database export feature.

--- app/src/main/java/io/github/dnloop/noteapp/ui/BackupFragment.kt
+++ app/src/main/java/io/github/dnloop/noteapp/ui/BackupFragment.kt
@@ -1,38 +1,108 @@
 package io.github.dnloop.noteapp.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import io.github.dnloop.noteapp.MainActivity
 import io.github.dnloop.noteapp.R
+import io.github.dnloop.noteapp.data.NoteDatabase
+import io.github.dnloop.noteapp.databinding.FragmentBackupBinding
 import io.github.dnloop.noteapp.ui.viewmodel.ShareViewModel
 
 class BackupFragment : Fragment() {
 
-    private lateinit var shareViewModel: ShareViewModel
+    private val _writeExternalStorageId = 1
+
+    private var _permission = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        shareViewModel =
-                ViewModelProvider(this).get(ShareViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_backup, container, false)
-        val textView: TextView = root.findViewById(R.id.text_share)
-        shareViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        val binding: FragmentBackupBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_backup, container, false)
+
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = NoteDatabase.getInstance(application)
+
+        val shareViewModel =
+            ViewModelProvider(this).get(ShareViewModel::class.java)
+
+        shareViewModel.setDataSource(dataSource)
+
+        requestStoragePermission()
+
+        if (ContextCompat.checkSelfPermission(this.activity!!, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            == PackageManager.PERMISSION_GRANTED) {
+            shareViewModel.setPermission(_permission)
+        }
+
+        binding.btnLocalExport.setOnClickListener {
+            if (!shareViewModel.exportLocalDatabase())
+                Toast.makeText(activity, "File system permissions required.", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLocalImport.setOnClickListener {
+            if (shareViewModel.importLocalDatabase())
+                Toast.makeText(activity, "Database Successfully export.", Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(activity, "File system permissions required.", Toast.LENGTH_SHORT).show()
+        }
+
+        return binding.root
+    }
+
+    private fun requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this.context!!,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this.activity!!,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                Toast.makeText(activity, "Permission needed to store database backup on the file system.", Toast.LENGTH_LONG).show()
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this.activity!!,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    _writeExternalStorageId
+                )
+            }
+        } else {
+            _permission = true
+        }
     }
 
     override fun onDetach() {
         super.onDetach()
         (activity as MainActivity?)?.getFloatingActionButton()?.show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            _writeExternalStorageId -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    _permission = true
+                }
+                return
+            }
+        }
+
     }
 }
--- app/src/main/java/io/github/dnloop/noteapp/ui/viewmodel/ShareViewModel.kt
+++ app/src/main/java/io/github/dnloop/noteapp/ui/viewmodel/ShareViewModel.kt
@@ -1,13 +1,71 @@
 package io.github.dnloop.noteapp.ui.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
+import io.github.dnloop.noteapp.data.NoteDatabase
+import timber.log.Timber
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.nio.channels.FileChannel
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
-class ShareViewModel : ViewModel() {
+class ShareViewModel(application: Application) :  AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is share Fragment"
+    private val _context = getApplication<Application>().applicationContext
+
+    private var _granted: Boolean = false
+
+    private lateinit var _currentDb: File
+
+    private lateinit var _dataSource: NoteDatabase
+
+    fun exportLocalDatabase(): Boolean {
+        return if (_granted) {
+            fileBackup()
+            true
+        } else {
+            false
+        }
     }
-    val text: LiveData<String> = _text
+
+    fun importLocalDatabase(): Boolean {
+        return if (_granted) {
+            true
+        } else {
+            false
+        }
+    }
+
+    fun setPermission(granted: Boolean) {
+        _granted = granted
+    }
+
+    fun setDataSource(dataSource: NoteDatabase) {
+        _dataSource = dataSource
+        _currentDb = _context.getDatabasePath(_dataSource.openHelper.databaseName)
+    }
+
+    private fun fileBackup() {
+        val dir = _context.getExternalFilesDir("Backup").toString()
+        val df: DateFormat = SimpleDateFormat("EEEd-MM-yyyy_HHmmss")
+        val date: String = df.format(Calendar.getInstance().time)
+        val fileName = "${_currentDb.name}-$date"
+        val backup = File(dir,fileName)
+        if (_currentDb.exists()) {
+            val src: FileChannel = FileInputStream(_currentDb).channel
+            val dst: FileChannel = FileOutputStream(backup).channel
+            dst.transferFrom(src, 0, src.size())
+            src.close()
+            dst.close()
+            Toast.makeText(_context, "Database Successfully exported to: $dir", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun restoreBackup() {
+
+    }
 }
